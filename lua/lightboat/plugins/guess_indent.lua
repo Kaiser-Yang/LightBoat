585f12cd0a3d2e439fb94d4c1affbfb35be10f57
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-26 10:57:37 +0800

    feat: add the guess_indent plugin

---
+++ lua/lightboat/plugins/guess_indent.lua
@@ -0,0 +1,24 @@
+return {
+    'NMAC427/guess-indent.nvim',
+    event = 'VeryLazy',
+    opts = {
+        filetype_exclude = {
+            'netrw',
+            'tutor',
+            'neo-tree',
+            'Avante',
+            'AvanteInput',
+        },
+        buftype_exclude = {
+            'help',
+            'nofile',
+            'terminal',
+            'prompt',
+        },
+        on_tab_options = {
+            expandtab = false,
+            tabstop = 4,
+            shiftwidth = 4,
+        },
+    },
+}
--- lua/lightboat/plugins/init.lua
+++ lua/lightboat/plugins/init.lua
@@ -3,4 +3,5 @@ return {
     { 'KaiserYang/LightBoat', lazy = false, priority = 10000, opts = {} },
     require('lightboat.plugins.treesitter'),
     require('lightboat.plugins.save'),
+    require('lightboat.plugins.guess_indent'),
 }
