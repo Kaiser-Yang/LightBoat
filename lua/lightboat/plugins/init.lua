dac67ccb88119e97eadc37422bbe8bba960b7c25
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-07-26 10:55:58 +0800

    feat: add the auto_save plugin

--- lua/lightboat/plugins/init.lua
+++ lua/lightboat/plugins/init.lua
@@ -2,4 +2,5 @@ require('lightboat.core').init()
 return {
     { 'KaiserYang/LightBoat', lazy = false, priority = 10000, opts = {} },
     require('lightboat.plugins.treesitter'),
+    require('lightboat.plugins.save'),
 }
---
+++ lua/lightboat/plugins/save.lua
@@ -0,0 +1,9 @@
+return {
+    'Pocco81/auto-save.nvim',
+    event = 'VeryLazy',
+    opts = {
+        execution_message = {
+            message = '',
+        },
+    },
+}
