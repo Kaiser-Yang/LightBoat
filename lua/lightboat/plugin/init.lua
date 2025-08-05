114504f1f7bee74f6bb5db4862b568cf79c30415
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-08-05 22:21:26 +0800

    fix: typo

--- lua/lightboat/plugin/init.lua
+++ lua/lightboat/plugin/init.lua
@@ -1,4 +1,4 @@
 require('lightboat.config').setup()
 require('lightboat.core').setup()
 
-return { { 'KaiserYang/LightBoat', lazy = false, priority = 99999, opts = {} } }
+return { { 'Kaiser-Yang/LightBoat', lazy = false, priority = 99999, opts = {} } }
