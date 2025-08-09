fce7753addcf9218c78847267bf8cab136d39e3d
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-08-09 23:06:14 +0800

    fix(render): wrong file_types

--- lua/lightboat/plugin/markdown/renderer.lua
+++ lua/lightboat/plugin/markdown/renderer.lua
@@ -8,7 +8,6 @@ local spec = {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     enabled = false,
-    file_types = vim.g.markdown_support_filetype,
     anti_conceal = { enabled = false },
     win_options = { concealcursor = { rendered = 'nvic' } },
     on = {
@@ -27,6 +26,7 @@ end
 M.setup = util.setup_check_wrap('lightboat.plugin.other.markdown', function()
   c = config.get().extra
   spec.ft = c.markdown_fts
+  spec.opts.file_types = c.markdown_fts
   return spec
 end, M.clear)
 
