d9bcb7f986ef1b8c074f57a3ab4c46bea63d2afb
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-08-10 13:06:16 +0800

    fix(noice): typo

--- lua/lightboat/plugin/ui/noice.lua
+++ lua/lightboat/plugin/ui/noice.lua
@@ -35,7 +35,7 @@ local spec = {
             { event = 'msg_show', find = 'ServiceReady' },
             { event = 'msg_show', find = 'Starting Java Language Server' },
             { event = 'msg_show', find = 'Init...' },
-            { find = 'Error running git%-blame: fatal: uable to access' },
+            { find = 'Error running git%-blame: fatal: unable to access' },
             { event = 'lsp', kind = 'progress', find = 'Building' },
             { event = 'lsp', kind = 'progress', find = 'Searching' },
             { event = 'lsp', kind = 'progress', find = 'Validate documents' },
