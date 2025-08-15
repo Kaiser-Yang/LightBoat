2f51c1702698d2a01db6934bd276675be3db618d
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-08-15 21:16:40 +0800

    feat(conform): disable in big files

--- lua/lightboat/config/conform/init.lua
+++ lua/lightboat/config/conform/init.lua
@@ -1,6 +1,6 @@
 return {
   enabled = true,
   keys = {
-    ['<leader>f'] = { key = '<leader>f', mode = { 'n', 'x' }, desc = 'Format' },
+    ['<leader>f'] = { key = '<leader>f', prev = 'big_file_check', mode = { 'n', 'x' }, desc = 'Format' },
   },
 }
