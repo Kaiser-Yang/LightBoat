c0906c8a09e504540ce41d4933c39a896a57a7df
Kaiser-Yang <kaiserqzyue@foxmail.com>
2025-08-24 13:47:40 +0800

    fix(noice): can not show highlight

--- lua/lightboat/plugin/ui/noice.lua
+++ lua/lightboat/plugin/ui/noice.lua
@@ -36,7 +36,7 @@ local spec = {
             any = {
               { event = 'msg_show', find = 'ServiceReady' },
               { event = 'msg_show', find = 'Starting Java Language Server' },
-              { event = 'msg_show', find = 'Init...' },
+              { event = 'msg_show', find = 'Init%.%.%.' },
               { find = 'Error running git%-blame: fatal: unable to access' },
               { find = 'Error running git%-blame: ssh: connect to host' },
               { event = 'lsp', kind = 'progress', find = 'Building' },
