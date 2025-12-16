768f21442d2d5606893660af1ecce8bd17ac1ba0
Kaiser-Yang <624626089@qq.com>
2025-12-16 21:53:14 +0800

    feat(neogit): add neogit plugin

--- lua/lightboat/plugin/git/init.lua
+++ lua/lightboat/plugin/git/init.lua
@@ -3,6 +3,7 @@ local name = 'lightboat.plugin.git'
 
 local M = {
   conflict = require('lightboat.plugin.git.conflict'),
+  neogit = require('lightboat.plugin.git.neogit'),
   octo = require('lightboat.plugin.git.octo'),
   sign = require('lightboat.plugin.git.sign'),
 }
---
+++ lua/lightboat/plugin/git/neogit.lua
@@ -0,0 +1,29 @@
+local M = {}
+local util = require('lightboat.util')
+local spec = {
+  'NeogitOrg/neogit',
+  lazy = true,
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'sindrets/diffview.nvim', -- optional - Diff integration
+  },
+  cmd = 'Neogit',
+  opts = {
+    mappings = {
+      status = {
+        ['Q'] = 'Close',
+      },
+    },
+  },
+  keys = {
+    { '<m-g>', '<cmd>Neogit<cr>', desc = 'Show Neogit UI' },
+  },
+}
+
+function M.spec() return spec end
+
+function M.clear() end
+
+M.setup = util.setup_check_wrap('lightboat.plugin.git.neogit', function() return spec end, M.clear)
+
+return M
