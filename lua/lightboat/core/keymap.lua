43354af30f6c6e6073971fe6c679cd8248e1b463
kaiser <kaiserqzyue@foxmail.com>
2026-01-24 19:08:33 +0800

    feat(keymap): support smart <home>

--- lua/lightboat/core/keymap.lua
+++ lua/lightboat/core/keymap.lua
@@ -68,7 +68,26 @@ local operation = {
     local res
     local mode = vim.fn.mode('1')
     if mode == 'c' then
-      res = '<home>'
+      local ok, noice_config = pcall(require, 'noice.config')
+      if not ok then return '<home>' end
+      local format = noice_config.defaults().cmdline.format
+      if not format then return '<home>' end
+      local line = vim.fn.getcmdtype() .. vim.fn.getcmdline()
+      local matched = nil
+      for _, v in pairs(format) do
+        local ps = type(v.pattern) == 'table' and v.pattern or { v.pattern }
+        for _, p in ipairs(ps) do
+          if p and type(p) == 'string' then
+            local cur_matched = line:match(p)
+            if not matched or cur_matched and #cur_matched > #matched then matched = cur_matched end
+          end
+        end
+      end
+      if matched then
+        res = '<home>' .. string.rep('<right>', #matched - 1)
+      else
+        res = '<home>'
+      end
     elseif mode == 'i' then
       res = '<c-o>^'
     else
