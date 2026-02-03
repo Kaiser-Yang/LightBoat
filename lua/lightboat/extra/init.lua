1ca4c8fb3aa15ae2f63df6a02f97643f40b5edf4
kaiser <kaiserqzyue@foxmail.com>
2026-02-03 10:23:58 +0800

    refactor(fold_sign): do not support now
    This functionality has many bugs.

--- lua/lightboat/extra/init.lua
+++ lua/lightboat/extra/init.lua
@@ -2,7 +2,6 @@ local util = require('lightboat.util')
 local M = {}
 
 function M.clear()
-  -- require('lightboat.extra.fold_sign').clear()
   require('lightboat.extra.command').clear()
   require('lightboat.extra.big_file').clear()
 end
@@ -10,7 +9,6 @@ end
 M.setup = util.setup_check_wrap('lightboat.extra', function()
   require('lightboat.extra.big_file').setup()
   require('lightboat.extra.command').setup()
-  -- require('lightboat.extra.fold_sign').setup()
 end, M.clear)
 
 return M
