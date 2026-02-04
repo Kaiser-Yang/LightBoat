f1056c3d388b2836f7ef2b0a540b6f973b043738
kaiser <kaiserqzyue@foxmail.com>
2026-02-04 22:40:07 +0800

    refactor(lualine)

--- lua/lightboat/plugin/ui/init.lua
+++ lua/lightboat/plugin/ui/init.lua
@@ -1,16 +1,8 @@
-local util = require('lightboat.util')
-local name = 'lightboat.plugin.ui'
-local M = {
+return {
   -- buffer_line = require('lightboat.plugin.ui.buffer_line'),
   -- catppuccin = require('lightboat.plugin.ui.catppuccin'),
-  -- lualine = require('lightboat.plugin.ui.lualine'),
+  require('lightboat.plugin.ui.lualine'),
   -- noice = require('lightboat.plugin.ui.noice'),
   -- statuscol = require('lightboat.plugin.ui.statuscol'),
   -- ufo = require('lightboat.plugin.ui.ufo'),
 }
-
-function M.clear() util.clear_plugins(M, name) end
-
-M.setup = util.setup_check_wrap(name, function() return util.setup_plugins(M, name) end, M.clear)
-
-return { M.setup() }
