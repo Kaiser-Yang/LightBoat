612bea32e9622c3d66013aca4ba0ee4582e23ba2
kaiser <kaiserqzyue@foxmail.com>
2026-02-07 23:51:44 +0800

    chore

--- lua/lightboat/plugin/code/lsp.lua
+++ lua/lightboat/plugin/code/lsp.lua
@@ -1,8 +1,11 @@
 return {
   {
     'neovim/nvim-lspconfig',
-    -- NOTE: Do not lazy load this plugin
+    -- NOTE:
+    -- Do not lazy load this plugin
     lazy = false,
+    -- NOTE:
+    -- We add mason here to make sure mason set paths of LSPs
     dependencies = { 'williamboman/mason.nvim' },
     cond = not vim.g.vscode,
     config = function()
