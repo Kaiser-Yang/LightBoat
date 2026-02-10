dd1657d6660acd0589d48f2830b98c96a63f0d89
kaiser <kaiserqzyue@foxmail.com>
2026-02-10 21:23:50 +0800

    chore: remove friendly-snippets

--- lua/lightboat/plugin/code/blink_cmp.lua
+++ lua/lightboat/plugin/code/blink_cmp.lua
@@ -3,16 +3,41 @@ return {
 
   'saghen/blink.cmp',
   cond = not vim.g.vscode,
-  dependencies = {
-    { 'rafamadriz/friendly-snippets', cond = not vim.g.vscode },
-  },
   version = '1.*',
   event = { 'InsertEnter', 'CmdlineEnter' },
   opts = {
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
       providers = {
-        buffer = { enabled = provider_enabled, name = 'Buff' },
+        buffer = {
+          enabled = provider_enabled,
+          name = 'Buff',
+          transform_items = function(context, items)
+            -- Do not convert case when searching
+            if context.mode == 'cmdline' then return items end
+            local out = {}
+            for _, item in ipairs(items) do
+              --- @type string
+              local raw = item.insertText
+              table.insert(out, item)
+              local item1 = vim.deepcopy(item)
+              item1.insertText, item1.label = raw:lower(), raw:lower()
+              table.insert(out, item1)
+              local item2 = vim.deepcopy(item)
+              item2.insertText, item2.label = raw:upper(), raw:upper()
+              table.insert(out, item2)
+              local item3 = vim.deepcopy(item)
+              item3.insertText, item3.label =
+                raw:sub(1, 1):upper() .. raw:sub(2):lower(), raw:sub(1, 1):upper() .. raw:sub(2):lower()
+              table.insert(out, item3)
+              local item4 = vim.deepcopy(item)
+              item4.insertText, item4.label =
+                raw:sub(1, 1):lower() .. raw:sub(2):upper(), raw:sub(1, 1):lower() .. raw:sub(2):upper()
+              table.insert(out, item4)
+            end
+            return out
+          end,
+        },
         lsp = { enabled = provider_enabled, fallbacks = {} },
         path = { opts = { show_hidden_files_by_default = true } },
         snippets = { name = 'Snip' },
