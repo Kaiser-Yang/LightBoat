32f374c6d90a8c975fecffacefd1ca354969becd
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-11 22:24:18 +0800

    chore: remove octo

--- lua/lightboat/plugin/git/init.lua
+++ lua/lightboat/plugin/git/init.lua
@@ -1,6 +1,5 @@
 return {
   require('lightboat.plugin.git.conflict'),
   -- diffview = require('lightboat.plugin.git.diffview'),
-  -- octo = require('lightboat.plugin.git.octo'),
   require('lightboat.plugin.git.sign'),
 }
