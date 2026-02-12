23ddb52f76d54e16da8fae590a551a954632e9f0
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-12 10:17:30 +0800

    chore: remove lualine

--- lua/lightboat/plugin/ui/init.lua
+++ lua/lightboat/plugin/ui/init.lua
@@ -1,7 +1,6 @@
 return {
   -- buffer_line = require('lightboat.plugin.ui.buffer_line'),
   -- catppuccin = require('lightboat.plugin.ui.catppuccin'),
-  require('lightboat.plugin.ui.lualine'),
   -- noice = require('lightboat.plugin.ui.noice'),
   -- statuscol = require('lightboat.plugin.ui.statuscol'),
   -- ufo = require('lightboat.plugin.ui.ufo'),
