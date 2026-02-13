8c1f3fdca26c43c5dad99d3f51efa62d3585ab2d
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-13 20:01:28 +0800

    chore: remove colorscheme

--- lua/lightboat/plugin/ui/init.lua
+++ lua/lightboat/plugin/ui/init.lua
@@ -1,6 +1,5 @@
 return {
   -- buffer_line = require('lightboat.plugin.ui.buffer_line'),
-  -- catppuccin = require('lightboat.plugin.ui.catppuccin'),
   -- noice = require('lightboat.plugin.ui.noice'),
   -- statuscol = require('lightboat.plugin.ui.statuscol'),
   -- ufo = require('lightboat.plugin.ui.ufo'),
