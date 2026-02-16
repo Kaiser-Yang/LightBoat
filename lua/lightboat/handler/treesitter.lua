7c650e18c427e65058e005623b71ff177a6bbbae
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-17 00:26:23 +0800

    fix(handler/treesitter): schedule wrap

--- lua/lightboat/handler/treesitter.lua
+++ lua/lightboat/handler/treesitter.lua
@@ -6,7 +6,10 @@ local textobject_available = u.plugin_available('nvim-treesitter-textobjects')
 --- @param position 'start'|'end'
 function M.go_to(direction, position, query_string, query_group)
   if not textobject_available or u.buffer.big() then return false end
-  require('nvim-treesitter-textobjects.move')['goto_' .. direction .. '_' .. position](query_string, query_group)
+  vim.schedule_wrap(require('nvim-treesitter-textobjects.move')['goto_' .. direction .. '_' .. position])(
+    query_string,
+    query_group
+  )
   -- HACK:
   -- We do not know if the operation is successful or not, so just return true
   return true
