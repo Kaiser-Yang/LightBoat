3e69940210ef346824496ada138601ad6314e027
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-16 12:41:02 +0800

    fix(markdown): undo break and wrong new line for code block

--- lua/lightboat/handler/init.lua
+++ lua/lightboat/handler/init.lua
@@ -105,7 +105,7 @@ M.markdown_bold = '<c-g>u<bs>****<++>' .. string.rep('<c-g>U<left>', 6)
 M.markdown_delete_line = '<c-g>u<bs>~~~~<++>' .. string.rep('<c-g>U<left>', 6)
 M.markdown_italic = '<c-g>u<bs>**<++>' .. string.rep('<c-g>U<left>', 5)
 M.markdown_math_block = '<c-g>u<bs>$$<cr><cr>$$<cr><cr><++>' .. string.rep('<up>', 3) .. string.rep('<right>', 2)
-M.markdown_code_block = '<c-g>u<bs>```<cr><cr>```<cr><cr><++>' .. string.rep('<up>', 4)
+M.markdown_code_block = '<c-g>u<bs>```<cr>```<cr><cr><++>' .. string.rep('<up>', 3)
 function M.markdown_goto_placeholder()
   local pattern = '<++>'
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -114,7 +114,7 @@ function M.markdown_goto_placeholder()
   local match = vim.fn.matchbufline(cur_buf, pattern, row, row_end)[1]
   if match then
     if match.lnum == row then
-      return '<bs>'
+      return '<c-g>u<bs>'
         .. string.rep('<c-g>U<right>', vim.fn.strchars(vim.api.nvim_get_current_line():sub(col + 1, match.byteidx)))
         .. string.rep('<del>', #pattern)
     else
