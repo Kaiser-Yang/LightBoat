59f03662a3fcb81b39d1e04a42de2dca44445581
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-16 22:50:49 +0800

    feat(handler/repmove)

--- lua/lightboat/handler/repmove.lua
+++ lua/lightboat/handler/repmove.lua
@@ -1,17 +1,110 @@
-local u = require('lightboat.util')
-local repmove_available = u.plugin_available('repmove')
-
 local M = {}
+local u = require('lightboat.util')
+-- HACK:
+-- Those below do not support vim.v.count
+local function next_section_start()
+  require('vim.treesitter._headings').jump({ count = 1 })
+  return true
+end
+local function previous_section_start()
+  require('vim.treesitter._headings').jump({ count = -1 })
+  return true
+end
+-- TODO:
+-- Find a better way to do this
+local previous_section_end = previous_section_start
+local next_section_end = next_section_start
+local go_to = require('lightboat.handler.treesitter').go_to
+
+-- HACK:
+-- Those below can not cycle
+local function next_loop_start() return go_to('next', 'start', '@loop.outer') end
+local function next_loop_end() return go_to('next', 'end', '@loop.outer') end
+local function next_class_start() return go_to('next', 'start', '@class.outer') end
+local function next_class_end() return go_to('next', 'end', '@class.outer') end
+local function next_block_start() return go_to('next', 'start', '@block.outer') end
+local function next_block_end() return go_to('next', 'end', '@block.outer') end
+local function next_return_start() return go_to('next', 'start', '@return.outer') end
+local function next_return_end() return go_to('next', 'end', '@return.outer') end
+local function next_conditional_start() return go_to('next', 'start', '@conditional.outer') end
+local function next_conditional_end() return go_to('next', 'end', '@conditional.outer') end
+local function next_function_start() return go_to('next', 'start', '@function.outer') end
+local function next_function_end() return go_to('next', 'end', '@function.outer') end
+local function next_parameter_start() return go_to('next', 'start', '@parameter.outer') end
+local function next_parameter_end() return go_to('next', 'end', '@parameter.outer') end
+local function next_statement_start() return go_to('next', 'start', '@statement.outer') end
+local function next_statement_end() return go_to('next', 'end', '@statement.outer') end
+local function next_call_start() return go_to('next', 'start', '@call.outer') end
+local function next_call_end() return go_to('next', 'end', '@call.outer') end
+local function previous_loop_start() return go_to('previous', 'start', '@loop.outer') end
+local function previous_loop_end() return go_to('previous', 'end', '@loop.outer') end
+local function previous_class_start() return go_to('previous', 'start', '@class.outer') end
+local function previous_class_end() return go_to('previous', 'end', '@class.outer') end
+local function previous_block_start() return go_to('previous', 'start', '@block.outer') end
+local function previous_block_end() return go_to('previous', 'end', '@block.outer') end
+local function previous_return_start() return go_to('previous', 'start', '@return.outer') end
+local function previous_return_end() return go_to('previous', 'end', '@return.outer') end
+local function previous_conditional_start() return go_to('previous', 'start', '@conditional.outer') end
+local function previous_conditional_end() return go_to('previous', 'end', '@conditional.outer') end
+local function previous_function_start() return go_to('previous', 'start', '@function.outer') end
+local function previous_function_end() return go_to('previous', 'end', '@function.outer') end
+local function previous_parameter_start() return go_to('previous', 'start', '@parameter.outer') end
+local function previous_parameter_end() return go_to('previous', 'end', '@parameter.outer') end
+local function previous_statement_start() return go_to('previous', 'start', '@statement.outer') end
+local function previous_statement_end() return go_to('previous', 'end', '@statement.outer') end
+local function previous_call_start() return go_to('previous', 'start', '@call.outer') end
+local function previous_call_end() return go_to('previous', 'end', '@call.outer') end
 
 -- stylua: ignore start
+local repmove_available = u.plugin_available('repmove.nvim')
 function M.comma() if not repmove_available then return ',' end return require('repmove').comma() end
 function M.semicolon() if not repmove_available then return ';' end return require('repmove').semicolon() end
 function M.F() return u.ensure_repmove('F', 'f', ',', ';')[1]() end
 function M.f() return u.ensure_repmove('F', 'f', ',', ';')[2]() end
 function M.T() return u.ensure_repmove('T', 't', ',', ';')[1]() end
 function M.t() return u.ensure_repmove('T', 't', ',', ';')[2]() end
-function M.previous_misspelled() return u.ensure_repmove('[s', ']s')[1]() end
 function M.next_misspelled() return u.ensure_repmove('[s', ']s')[2]() end
+function M.next_section_start() return u.ensure_repmove(previous_section_start, next_section_start)[2]() end
+function M.next_section_end() return u.ensure_repmove(previous_section_end, next_section_end)[2]() end
+function M.next_function_start() return u.ensure_repmove(previous_function_start, next_function_start)[2]() end
+function M.next_function_end() return u.ensure_repmove(previous_function_end, next_function_end)[2]() end
+function M.next_class_start() return u.ensure_repmove(previous_class_start, next_class_start)[2]() end
+function M.next_class_end() return u.ensure_repmove(previous_class_end, next_class_end)[2]() end
+function M.next_block_end() return u.ensure_repmove(previous_block_end, next_block_end)[2]() end
+function M.next_block_start() return u.ensure_repmove(previous_block_start, next_block_start)[2]() end
+function M.next_loop_start() return u.ensure_repmove(previous_loop_start, next_loop_start)[2]() end
+function M.next_loop_end() return u.ensure_repmove(previous_loop_end, next_loop_end)[2]() end
+function M.next_return_start() return u.ensure_repmove(previous_return_start, next_return_start)[2]() end
+function M.next_return_end() return u.ensure_repmove(previous_return_end, next_return_end)[2]() end
+function M.next_parameter_start() return u.ensure_repmove(previous_parameter_start, next_parameter_start)[2]() end
+function M.next_parameter_end() return u.ensure_repmove(previous_parameter_end, next_parameter_end)[2]() end
+function M.next_conditional_start() return u.ensure_repmove(previous_conditional_start, next_conditional_start)[2]() end
+function M.next_conditional_end() return u.ensure_repmove(previous_conditional_end, next_conditional_end)[2]() end
+function M.next_statement_start() return u.ensure_repmove(previous_statement_start, next_statement_start)[2]() end
+function M.next_statement_end() return u.ensure_repmove(previous_statement_end, next_statement_end)[2]() end
+function M.next_call_start() return u.ensure_repmove(previous_call_start, next_call_start)[2]() end
+function M.next_call_end() return u.ensure_repmove(previous_call_end, next_call_end)[2]() end
+function M.previous_misspelled() return u.ensure_repmove('[s', ']s')[1]() end
+function M.previous_section_start() return u.ensure_repmove(previous_section_start, next_section_start)[1]() end
+function M.previous_section_end() return u.ensure_repmove(previous_section_end, next_section_end)[1]() end
+function M.previous_function_start() return u.ensure_repmove(previous_function_start, next_function_start)[1]() end
+function M.previous_function_end() return u.ensure_repmove(previous_function_end, next_function_end)[1]() end
+function M.previous_class_start() return u.ensure_repmove(previous_class_start, next_class_start)[1]() end
+function M.previous_class_end() return u.ensure_repmove(previous_class_end, next_class_end)[1]() end
+function M.previous_block_start() return u.ensure_repmove(previous_block_start, next_block_start)[1]() end
+function M.previous_block_end() return u.ensure_repmove(previous_block_end, next_block_end)[1]() end
+function M.previous_loop_start() return u.ensure_repmove(previous_loop_start, next_loop_start)[1]() end
+function M.previous_loop_end() return u.ensure_repmove(previous_loop_end, next_loop_end)[1]() end
+function M.previous_return_start() return u.ensure_repmove(previous_return_start, next_return_start)[1]() end
+function M.previous_return_end() return u.ensure_repmove(previous_return_end, next_return_end)[1]() end
+function M.previous_parameter_start() return u.ensure_repmove(previous_parameter_start, next_parameter_start)[1]() end
+function M.previous_parameter_end() return u.ensure_repmove(previous_parameter_end, next_parameter_end)[1]() end
+function M.previous_conditional_start() return u.ensure_repmove(previous_conditional_start, next_conditional_start)[1]() end
+function M.previous_conditional_end() return u.ensure_repmove(previous_conditional_end, next_conditional_end)[1]() end
+function M.previous_statement_start() return u.ensure_repmove(previous_statement_start, next_statement_start)[1]() end
+function M.previous_statement_end() return u.ensure_repmove(previous_statement_end, next_statement_end)[1]() end
+function M.previous_call_start() return u.ensure_repmove(previous_call_start, next_call_start)[1]() end
+function M.previous_call_end() return u.ensure_repmove(previous_call_end, next_call_end)[1]() end
 -- stylua: ignore end
 
 return M
---
+++ lua/lightboat/handler/treesitter.lua
@@ -0,0 +1,15 @@
+local u = require('lightboat.util')
+local M = {}
+
+local textobject_available = u.plugin_available('nvim-treesitter-textobjects')
+--- @param direction 'next'|'previous'
+--- @param position 'start'|'end'
+function M.go_to(direction, position, query_string, query_group)
+  if not textobject_available or u.buffer.big() then return false end
+  require('nvim-treesitter-textobjects.move')['goto_' .. direction .. '_' .. position](query_string, query_group)
+  -- HACK:
+  -- We do not know if the operation is successful or not, so just return true
+  return true
+end
+
+return M
