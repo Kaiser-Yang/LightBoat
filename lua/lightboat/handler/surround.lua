38b4ffdf8a0e1bc1aec79aa7b970618b8e87da9a
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-17 15:33:56 +0800

    chore(handler/surround): do not expose some functions

--- lua/lightboat/handler/surround.lua
+++ lua/lightboat/handler/surround.lua
@@ -3,6 +3,52 @@ local M = {}
 
 local last_count = 1
 local surround_available = u.plugin_available('nvim-surround')
+local l = {}
+function l.surround_normal()
+  if not surround_available then return false end
+  u.ensure_plugin('nvim-surround')
+  return '<plug>(nvim-surround-normal)'
+end
+function l.surround_normal_current()
+  if not surround_available then return false end
+  u.ensure_plugin('nvim-surround')
+  return '<plug>(nvim-surround-normal-cur)'
+end
+function l.surround_normal_line()
+  if not surround_available then return false end
+  u.ensure_plugin('nvim-surround')
+  return '<plug>(nvim-surround-normal-line)'
+end
+function l.surround_normal_current_line()
+  if not surround_available then return false end
+  u.ensure_plugin('nvim-surround')
+  return '<plug>(nvim-surround-normal-cur-line)'
+end
+function l.surround_insert()
+  if not surround_available then return false end
+  u.ensure_plugin('nvim-surround')
+  return '<plug>(nvim-surround-insert)'
+end
+function l.surround_insert_line()
+  if not surround_available then return false end
+  u.ensure_plugin('nvim-surround')
+  return '<plug>(nvim-surround-insert-line)'
+end
+function l.surround_delete()
+  if not surround_available then return false end
+  u.ensure_plugin('nvim-surround')
+  return '<plug>(nvim-surround-delete)'
+end
+function l.surround_change()
+  if not surround_available then return false end
+  u.ensure_plugin('nvim-surround')
+  return '<plug>(nvim-surround-change)'
+end
+function l.surround_change_line()
+  if not surround_available then return false end
+  u.ensure_plugin('nvim-surround')
+  return '<plug>(nvim-surround-change-line)'
+end
 local function hack(suffix, is_S)
   if not surround_available then return false end
   suffix = suffix or ''
@@ -12,16 +58,16 @@ local function hack(suffix, is_S)
   if op == 'y' then
     if is_S then
       -- suffix is ignored when is_S this will make "yS" behaviour like "ys$"
-      res = M['surround_normal']
+      res = l['surround_normal']
     else
-      res = M['surround_normal' .. suffix]
+      res = l['surround_normal' .. suffix]
     end
   elseif op == 'd' then
-    res = M['surround_delete' .. suffix]
+    res = l['surround_delete' .. suffix]
   elseif op == 'c' then
-    res = M['surround_change' .. suffix]
+    res = l['surround_change' .. suffix]
   elseif op == 'g@' and vim.o.operatorfunc:find('nvim%-surround') then
-    res = M['surround_normal_current' .. suffix]
+    res = l['surround_normal_current' .. suffix]
   end
   if not res then return false end
   if op ~= 'g@' then
@@ -31,51 +77,6 @@ local function hack(suffix, is_S)
   end
 end
 
-function M.surround_normal()
-  if not surround_available then return false end
-  u.ensure_plugin('nvim-surround')
-  return '<plug>(nvim-surround-normal)'
-end
-function M.surround_normal_current()
-  if not surround_available then return false end
-  u.ensure_plugin('nvim-surround')
-  return '<plug>(nvim-surround-normal-cur)'
-end
-function M.surround_normal_line()
-  if not surround_available then return false end
-  u.ensure_plugin('nvim-surround')
-  return '<plug>(nvim-surround-normal-line)'
-end
-function M.surround_normal_current_line()
-  if not surround_available then return false end
-  u.ensure_plugin('nvim-surround')
-  return '<plug>(nvim-surround-normal-cur-line)'
-end
-function M.surround_insert()
-  if not surround_available then return false end
-  u.ensure_plugin('nvim-surround')
-  return '<plug>(nvim-surround-insert)'
-end
-function M.surround_insert_line()
-  if not surround_available then return false end
-  u.ensure_plugin('nvim-surround')
-  return '<plug>(nvim-surround-insert-line)'
-end
-function M.surround_delete()
-  if not surround_available then return false end
-  u.ensure_plugin('nvim-surround')
-  return '<plug>(nvim-surround-delete)'
-end
-function M.surround_change()
-  if not surround_available then return false end
-  u.ensure_plugin('nvim-surround')
-  return '<plug>(nvim-surround-change)'
-end
-function M.surround_change_line()
-  if not surround_available then return false end
-  u.ensure_plugin('nvim-surround')
-  return '<plug>(nvim-surround-change-line)'
-end
 function M.surround_visual()
   if not surround_available then return false end
   u.ensure_plugin('nvim-surround')
