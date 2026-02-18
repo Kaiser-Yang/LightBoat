8f975dae4c6db2ba230f80d7c4ef40c94778cc09
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-18 18:18:25 +0800

    chore

--- lua/lightboat/plugin/ui/context.lua
+++ lua/lightboat/plugin/ui/context.lua
@@ -2,5 +2,8 @@ return {
   'nvim-treesitter/nvim-treesitter-context',
   cond = not vim.g.vscode,
   lazy = false,
-  opts = { on_attach = function(buffer) return not require('lightboat.util').buffer.big(buffer) end },
+  opts = {
+    max_liens = vim.o.scrolloff,
+    on_attach = function(buffer) return not require('lightboat.util').buffer.big(buffer) end,
+  },
 }
