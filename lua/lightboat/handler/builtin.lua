cb58436265804bb40447dd1df993ab328dda0d4e
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-22 00:39:47 +0800

    feat(lazygit): builtin support

--- lua/lightboat/handler/builtin.lua
+++ lua/lightboat/handler/builtin.lua
@@ -182,6 +182,60 @@ function M.delete_to_eol()
   end
 end
 
+local lazygit_win = nil
+local lazygit_buf = nil
+local original_cmdheight = nil
+function M.toggle_lazygit()
+  if vim.fn.executable('lazygit') == 0 then
+    vim.notify('lazygit is not installed or not in PATH', vim.log.levels.WARN, { title = 'Light Boat' })
+    return false
+  end
+  if lazygit_win and vim.api.nvim_win_is_valid(lazygit_win) then
+    vim.api.nvim_win_hide(lazygit_win)
+  else
+    if not lazygit_buf or not vim.api.nvim_buf_is_valid(lazygit_buf) then
+      lazygit_buf = vim.api.nvim_create_buf(false, true)
+    end
+    local width = vim.o.columns
+    local height = vim.o.lines
+    lazygit_win = vim.api.nvim_open_win(lazygit_buf, true, {
+      relative = 'editor',
+      width = width,
+      height = height,
+      row = 0,
+      col = 0,
+      zindex = 50,
+      style = 'minimal',
+      border = 'none',
+    })
+    if lazygit_win == 0 then
+      vim.notify('Failed to open lazygit window', vim.log.levels.ERROR, { title = 'Light Boat' })
+      return false
+    end
+    vim.wo[lazygit_win].cursorcolumn = false
+    vim.wo[lazygit_win].signcolumn = 'no'
+    vim.wo[lazygit_win].winhl = 'NormalFloat:Normal'
+    if vim.bo[lazygit_buf].buftype ~= 'terminal' then
+      vim.cmd('terminal lazygit')
+      vim.bo[lazygit_buf].filetype = 'lazygit'
+      vim.bo[lazygit_buf].buflisted = false
+      vim.bo[lazygit_buf].swapfile = false
+      vim.bo[lazygit_buf].bufhidden = 'hide'
+    end
+    original_cmdheight = vim.o.cmdheight
+    vim.o.cmdheight = 0
+    vim.api.nvim_create_autocmd('BufLeave', {
+      buffer = lazygit_buf,
+      once = true,
+      callback = function()
+        vim.o.cmdheight = original_cmdheight
+        original_cmdheight = nil
+      end,
+    })
+    vim.cmd('startinsert')
+  end
+end
+
 function M.cursor_to_eol()
   local mode = vim.api.nvim_get_mode().mode
   if mode:sub(1, 1) == 'i' then
