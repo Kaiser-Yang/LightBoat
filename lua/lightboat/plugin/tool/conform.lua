b168837ec10c2af7eaa292c78803f78ab37f2a68
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-02-27 23:05:48 +0800

    chore: do not notify when there is no formaters available

--- lua/lightboat/plugin/tool/conform.lua
+++ lua/lightboat/plugin/tool/conform.lua
@@ -2,7 +2,7 @@ return {
   'stevearc/conform.nvim',
   cond = not vim.g.vscode,
   opts = {
-    notify_no_formatters = true,
+    notify_no_formatters = false,
     formatters_by_ft = { lua = { 'stylua' } },
     default_format_opts = { lsp_format = 'fallback', stop_after_first = true },
   },
