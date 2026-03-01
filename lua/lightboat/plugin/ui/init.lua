27969268a296781ad943993b847f7fa24451f06e
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-03-01 22:36:52 +0800

    chore: remove lualine.nvim

--- lua/lightboat/plugin/ui/init.lua
+++ lua/lightboat/plugin/ui/init.lua
@@ -1,7 +1,6 @@
 return {
   require('lightboat.plugin.ui.blink_indent'),
   require('lightboat.plugin.ui.context'),
-  require('lightboat.plugin.ui.lualine'),
   require('lightboat.plugin.ui.nui'),
   require('lightboat.plugin.ui.todo_comment'),
 }
