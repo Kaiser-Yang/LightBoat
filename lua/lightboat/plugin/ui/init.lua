2b19a31f1d339ec17bceaed8f13ada2b23618f2b
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-03-01 23:08:56 +0800

    Revert "chore: remove lualine.nvim"
    This reverts commit 27969268a296781ad943993b847f7fa24451f06e.

--- lua/lightboat/plugin/ui/init.lua
+++ lua/lightboat/plugin/ui/init.lua
@@ -1,6 +1,7 @@
 return {
   require('lightboat.plugin.ui.blink_indent'),
   require('lightboat.plugin.ui.context'),
+  require('lightboat.plugin.ui.lualine'),
   require('lightboat.plugin.ui.nui'),
   require('lightboat.plugin.ui.todo_comment'),
 }
