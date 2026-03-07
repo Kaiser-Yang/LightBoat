2eb6621faf134561114a9221b279f9d14611de12
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-03-07 15:56:26 +0800

    Revert "chore: disable auto brackets"
    This reverts commit c0a621272a7aa1e1133aa655dbc94286a564a171.

--- lua/lightboat/plugin/edit/blink_cmp.lua
+++ lua/lightboat/plugin/edit/blink_cmp.lua
@@ -67,7 +67,6 @@ return {
     },
     keymap = { preset = 'none' },
     completion = {
-      accept = { auto_brackets = { enabled = false } },
       menu = {
         scrolloff = 0,
         max_height = 15,
