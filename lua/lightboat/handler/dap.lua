b1b0bd52484215de0795764f30cfa560517a7c76
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-03-10 21:57:47 +0800

    feat(dap): more hanlders

--- lua/lightboat/handler/dap.lua
+++ lua/lightboat/handler/dap.lua
@@ -1,18 +1,40 @@
 local M = {}
 local u = require('lightboat.util')
 
-function M.toggle_dap_view()
-  local dap_view = require('dap-view')
-  local dap_win_num = #u.buffer.get_win_with_filetype('dap%-view')
-  if dap_win_num == 0 and _G.plugin_loaded['nvim-tree.lua'] then
-    local tree = require('nvim-tree.api').tree
-    if tree.is_visible() then tree.toggle() end
+local function check()
+  if not u.plugin_available('nvim-dap') then
+    vim.notify('nvim-dap is not available', vim.log.levels.WARN, { title = 'Light Boat' })
+    return false
   end
-  dap_view.toggle()
+  return true
+end
+
+local function check_dap_view()
+  if not u.plugin_available('nvim-dap-view') then
+    vim.notify('nvim-dap-view is not available', vim.log.levels.WARN, { title = 'Light Boat' })
+    return false
+  end
+end
+
+function M.toggle_dap_view()
+  if not check_dap_view() then return false end
+  require('dap-view').toggle()
+  return true
+end
+
+function M.restart_or_run_last()
+  if not check() then return false end
+  local dap = require('dap')
+  if dap.session() then
+    dap.restart()
+  else
+    dap.run_last()
+  end
   return true
 end
 
 M.set_condition_breakpoint = function()
+  if not check() then return false end
   vim.ui.input({ prompt = 'Condition Breakpoint' }, function(input)
     if input and input ~= '' then require('dap').set_breakpoint(input) end
   end)
@@ -20,6 +42,7 @@ M.set_condition_breakpoint = function()
 end
 
 M.set_log_point = function()
+  if not check() then return false end
   vim.ui.input({ prompt = 'Log Point Message' }, function(input)
     if input and input ~= '' then require('dap').set_breakpoint(nil, nil, input) end
   end)
