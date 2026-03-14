6585609d5a40801bb844cb609c4217efa92fdeba
Kaiser-Yang <kaiserqzyue@foxmail.com>
2026-03-14 16:59:22 +0800

    Revert "chore: remove dependencies when possible"
    This reverts commit 321c09ecf77a160bc04ad9738409298c6f34fe42.

--- lua/lightboat/plugin/edit/pair.lua
+++ lua/lightboat/plugin/edit/pair.lua
@@ -1,13 +1,11 @@
 vim.g.nvim_surround_no_mappings = true
-local has_cargo = vim.fn.executable('cargo') == 1
 return {
   {
     'saghen/blink.pairs',
-    version = has_cargo and nil or '*',
-    build = has_cargo and 'cargo build --release' or nil,
+    version = '*',
     cond = not vim.g.vscode,
     event = 'VeryLazy',
-    dependencies = has_cargo and nil or 'saghen/blink.download',
+    dependencies = 'saghen/blink.download',
     opts = {
       mappings = {
         enabled = false,
